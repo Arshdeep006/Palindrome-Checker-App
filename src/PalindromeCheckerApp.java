df476003ea7e75e47cea1bb34ab0b5fc551bdfa8
Arshdeep006 <singharshdepp59@gmail.com>
2026-02-25 10:25:28 +0530

    UC4:Hardcode

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,30 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-        String reversed = "";
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a string: ");
+            String value = scanner.nextLine();
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
+            char[] letters = value.toCharArray();
+            int left = 0;
+            int right = letters.length - 1;
+            boolean palindrome = true;
 
-        if (input.equals(reversed)) {
-            System.out.println("The entered string is a palindrome");
-        } else {
-            System.out.println("The entered string is not a palindrome");
-        }
+            while (left < right) {
+                if (letters[left] != letters[right]) {
+                    palindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
 
-        sc.close();
+            if (palindrome) {
+                System.out.println("The entered string is a palindrome");
+            } else {
+                System.out.println("The entered string is not a palindrome");
+            }
+
+            scanner.close();git add .
 }
 }
