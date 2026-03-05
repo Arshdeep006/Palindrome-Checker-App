bb88e4d796a657cff0841147d5f5601e3bb3a1ee
Arshdeep006 <singharshdepp59@gmail.com>
2026-03-05 10:18:48 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,33 +2,7 @@ public class PalindromeCheckerApp{
 
     public static void main(String[] args) {
 
-        String input = "level";
-
-        long startTime = System.nanoTime();
-        boolean result = checkUsingStack(input);
-        long endTime = System.nanoTime();
-
-        long executionTime = endTime - startTime;
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
-        System.out.println("Execution Time : " + executionTime + " ns");
-    }
-
-    public static boolean checkUsingStack(String input) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
+        System.out.println("Welcome to Palindrome Checker!");
+        System.out.println("Version~1.0");
     }
 }
